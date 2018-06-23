d3a98de2ab654f566b4cb180b2db6fad86b70092
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-23 11:36:05 -0400

    Entered 'a' input status

--- db_worklog.py
+++ db_worklog.py
@@ -27,10 +27,34 @@ def main():
 	'''
 	clear_screen()
 	inpt = screen_prompt(
-				'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program', 
+				'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\n' +
+				'c) Enter new employee\nd) Quit program', 
 				'>', 
-				'[AaBbCc]'
+				'[AaBbCcDd]'
 				)
+				
+	if inpt.lower()=='a':
+			clear_screen()
+			#Retrieve the employee id
+			id = screen_prompt('Employee ID: ', '', '\d*')
+			
+			clear_screen()
+			#display the date task screen and retrieve the date.
+			date = screen_prompt('Date of the task\nPlease use MM/DD/YYYY: ', '', '([0-1][0-9])\/([0-3][0-9])\/[0-9]{4}')
+			
+			clear_screen()
+			#Retrieve the title of the task
+			name = screen_prompt('Name of the task: ', '>', '.*[\w\s].*')
+			
+			clear_screen()
+			#Retrieve the time spent completing the task
+			time = screen_prompt('Time Spent (rounded by minute): ', '>', '\d+')
+			
+			clear_screen()
+			#Prompt for notes
+			notes = screen_prompt('Notes (optional, allowed to leave blank): ', '>', '.*[\w\s].*')
+			
+			#TODO: Enter task to task database
 
 if __name__ == '__main__':
 	main()
