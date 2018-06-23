c9ea0149449be02f4443948ee4892d1a1e2115d8
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-23 09:35:52 -0400

    Added task screen file

--- db_worklog.py
+++ db_worklog.py
@@ -3,3 +3,28 @@ import csv
 import os
 import re
 import sys
+
+def clear_screen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	
+def screen_prompt(display, input, regex):
+	'''
+		Displays a screen message and compares input
+		against a regex pattern.
+	'''
+	task = Task_Screen(display, input, regex)
+	task.display()
+	return task.input()
+	
+def main():
+	'''
+		Displays the main menu and prompts
+		user for a choice to either add a task, 
+		search for a task, or to exit the program.
+	'''
+	clear_screen()
+	inpt = screen_prompt(
+				'WORK LOG\nWhat would you like to do?\na) Add a new entry\nb) Search in existing entries\nc) Quit program', 
+				'>', 
+				'[AaBbCc]'
+				)
---
+++ task_screen.py
@@ -0,0 +1,41 @@
+'''Task Screen'''
+#Task Screen
+import os
+import re
+
+class Task_Screen:
+
+	def __init__(self, display_message, input_prompt, answers):
+		'''
+			Initializes a Task_Screen object by setting
+			a display message, an input prompt, and a regex
+			pattern to compare answers against.
+		'''
+		self.display_message = display_message
+		self.input_prompt = input_prompt
+		self.answers = answers
+		
+	def clear_screen(self):
+		os.system('cls' if os.name == 'nt' else 'clear')
+		
+	def display(self):
+		'''
+			Displays the message of the object.
+		'''
+		self.clear_screen()
+		print(self.display_message)
+		
+
+	def input(self):
+		'''
+			Continues to prompt user for input until
+			the input matches the regex pattern.
+		'''
+		while True:
+			ans = input(self.input_prompt)
+			if not re.match(self.answers, ans):
+				print('Please enter a valid entry.')
+				continue
+			else:
+				return ans
+				
