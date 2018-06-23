c29a418d6c3b0fd39750d70230eec6fe854332ea
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-23 11:27:01 -0400

    Created Task and Employee databases

--- employee_db.py
+++ employee_db.py
@@ -1,1 +1,21 @@
 '''employee database'''
+from peewee import *
+
+edb = SqliteDatabase('employees.db')
+tdb = SqliteDatabase('tasks.db')
+
+class Employee(Model):
+	emp_id = IntegerField
+	first_name = CharField(max_length=255)
+	last_name = CharField(max_length=255)
+	class Meta:
+		database = edb
+	
+class Task(Model):
+	emp_id = IntegerField()
+	date = DateTimeField()
+	title = CharField(max_length=255)
+	time = IntegerField()
+	notes = TextField()
+	class Meta:
+		database = tdb
