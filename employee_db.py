540ad9d173aa58f67a72ec30e80ffc4965d6d663
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-25 08:14:26 -0400

    Made Task class point to edb database

--- employee_db.py
+++ employee_db.py
@@ -2,20 +2,19 @@
 from peewee import *
 
 edb = SqliteDatabase('employees.db')
-tdb = SqliteDatabase('tasks.db')
 
 class Employee(Model):
-	emp_id = IntegerField
+	emp_id = IntegerField(unique=True)
 	first_name = CharField(max_length=255)
 	last_name = CharField(max_length=255)
 	class Meta:
 		database = edb
 	
 class Task(Model):
-	emp_id = IntegerField()
+	emp_id = IntegerField(unique=True)
 	date = DateTimeField()
 	title = CharField(max_length=255)
 	time = IntegerField()
 	notes = TextField()
 	class Meta:
-		database = tdb
+		database = edb
