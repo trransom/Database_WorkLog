0bba5bfc0fecc6c8aee0aaa74aa7590fe356351e
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-06-27 10:15:02 -0400

    Added test for test_screen init

--- tests.py
+++ tests.py
@@ -2,5 +2,19 @@
 import unittest
 
 import db_worklog
-import task_screen
+from task_screen import Task_Screen
+
+
+class WorklogTests(unittest.TestCase):
+	def test_ts_init(self):
+		d = 'display'
+		i = 'input'
+		r = '[A-Za-z]'
+		test = Task_Screen('display', 'input', '[A-Za-z]')
+		assert d == test.display_message
+		assert i == test.input_prompt
+		assert r == test.answers
+		
+if __name__ == '__main__':
+	unittest.main()
 
